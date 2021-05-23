9b3a489d14016067d949e52ed7f687e117e000ca
ashvegeta <ashik2001mp@gmail.com>
2021-05-23 20:16:57 +0530

    modified async.js

--- async.js
+++ async.js
@@ -2,12 +2,14 @@
 
 const {readFile} = require('fs')
 
+console.log('starting first task')
+
 readFile('./content/subfolder/write.txt','utf8',(err , result) => {
     if(err){
         console.log(err)
         return
     }
-    console.log('fninshed reading')
+    console.log('finished reading')
 })
 
 console.log('starting with next task')
